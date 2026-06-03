47cde8b1cb8885ae7f61c4079bc13bc0057eb8ba
Hamathx999 <hamath.o.diallo18@gmail.com>
2026-06-03 13:38:36 +0000

    ajout des pages de gestion pour les admins !

---
+++ src/main/java/com/denkolochi/servlet/ServletDashboardAdmin.java
@@ -0,0 +1,41 @@
+
+package com.denkolochi.servlet;
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servlet
+ */
+@WebServlet("/servlet")
+public class ServletDashboardAdmin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletDashboardAdmin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/views/Dashboard/dashboard-admin.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
